27fa6ac47dbf3dacdc208c736459476ef65532d5
takami.kosuke <takamikousuke@gmail.com>
2011-08-07 14:26:20 +0900

    gameover処理追加

--- game.js
+++ game.js
@@ -14,6 +14,33 @@
 		];
 		var PANEL_X_SIZE = 64;
 		var PANEL_Y_SIZE = 64;
+
+		var PanelType = Class.create({
+			initialize: function() {
+				this.COIN      = 2;
+				this.POTION    = 3;
+				this.SHIELD    = 4;
+				this.SKELTON   = 5;
+				this.SWORD     = 6;
+			}
+		});
+
+		var PANEL_TYPE   = new PanelType();
+
+		var Direction = Class.create({
+			initialize: function(){
+				this.RIGHT     = 0;
+				this.DOWN      = 1;
+				this.LEFT      = 2;
+				this.TOP       = 3;
+				this.RIGHTDOWN = 4;
+				this.LEFTDOWN  = 5;
+				this.LEFTTOP   = 6;
+				this.RIGHTTOP  = 7;
+			}
+		});
+
+		var DIRECTION = new Direction();
 		var DEBUG_MODE   = true;
 		game.preload( resources );
 
@@ -21,7 +48,6 @@
 			initialize: function( game, nextScene ) {
 				Scene.apply( this, arguments );
 				this.game       = game;
-				this.nextScene  = nextScene;
 				this.onEnter();
 			},
 			onEnter: function() {
@@ -29,6 +55,29 @@
 				this.gamestart  = new Label( "Game Start" );
 				this.gamestart.moveTo( 0, 20 );
 				this.gamestart.addEventListener('touchstart', bind( function(e) {
+					this.nextScene  = new GameScene( game );
+					this.game.replaceScene( this.nextScene );
+					this.onExit();
+				}, this ));
+				this.addChild(this.title);
+				this.addChild(this.gamestart);
+			},
+			onExit: function(){
+			}
+		});
+
+		var GameOverScene = Class.create( Scene, {
+			initialize: function( game, nextScene ) {
+				Scene.apply( this, arguments );
+				this.game       = game;
+				this.onEnter();
+			},
+			onEnter: function() {
+				this.title      = new Label( "Game Over" );
+				this.gamestart  = new Label( "Retry" );
+				this.gamestart.moveTo( 0, 20 );
+				this.gamestart.addEventListener('touchstart', bind( function(e) {
+					this.nextScene  = new GameScene( game );
 					this.game.replaceScene( this.nextScene );
 					this.onExit();
 				}, this ));
@@ -43,17 +92,104 @@
 			initialize: function( game, nextScene ) {
 				Scene.apply( this, arguments );
 				this.game       = game;
-				this.nextScene  = nextScene;
 				this.onEnter();
 			},
 			onEnter: function() {
 				this.title = new Label( "Game Main" );
 				this.addChild(this.title);
+				this.turnManager = new TurnManager( bind( this.onGameOver, this ) );
+				this.turnLabel   = new Label();
+				this.turnLabel.moveTo( 100, 0 );
+				this.addEventListener( 'enterframe', this.onUpdate );
 				this.Field      = new Field();
 				this.Field.moveTo( 0, 20 );
+				this.Field.addTurnManager( this.turnManager );
+				this.Player     = new Player();
+				this.lifeLabel  = new Label();
+				this.lifeLabel.moveTo( 200, 0 );
+				this.addChild( this.turnLabel );
+				this.addChild( this.lifeLabel );
 				this.addChild( this.Field );
+				this.addChild( this.Player );
+				this.turnManager.addField( this.Field );
+				this.turnManager.addPlayer( this.Player );
+				this.turnManager.startTurn();
 			},
+
+			onUpdate: function() {
+				this.turnLabel.text = "turn:" + this.turnManager.getTurn();
+				this.lifeLabel.text = "life:" + this.Player.getLife();
+			},
+
+			onGameOver: function() {
+				this.nextScene  = new GameOverScene( game );
+				this.game.replaceScene( this.nextScene );
+			},
+
 			onExit: function() {
+			}
+		});
+
+		var TurnManager = Class.create({
+			initialize: function( onGameOver ) {
+				this.turn       = 0;
+				this.onGameOver = onGameOver;
+			},
+
+			getTurn: function() {
+				return this.turn;
+			},
+
+			addField: function( field ) {
+				this.Field = field;
+			},
+
+			addPlayer: function( player ) {
+				this.Player = player;
+			},
+
+
+			startTurn: function() {
+				this.onEnterTurn();
+			},
+
+			endTurn: function() {
+				var damage = this.Field.getDamageFromEnemies();
+				this.Player.reduceLife( damage );
+				if ( this.Player.getLife() <= 0) {
+					this.onGameOver();
+				}
+				this.onExitTurn();
+			},
+
+			onEnterTurn: function() {
+			},
+
+			onCancelTouch: function() {
+				this.Field.clear();
+			},
+
+			onExitTurn: function() {
+				this.turn++;
+				//ターンが終了したときに呼ばれる。
+				//終了処理
+				this.Field.closeUp();
+				this.Field.clear();
+				this.startTurn();
+			}
+		});
+
+		var Player   = Class.create( Group, {
+			initialize: function() {
+				Group.apply( this, arguments );
+				this.life = 10;
+			},
+			getLife: function() {
+				return this.life;
+			},
+			reduceLife: function( dLife ) {
+				if ( dLife === undefined ) { return; }
+				this.life -= dLife;
 			}
 		});
 
@@ -85,6 +221,11 @@
 				this.addEventListener( 'touchend', this.onTouchEnd );
 				this.addEventListener( 'touchmove', this.onTouchMove );
 			},
+
+			addTurnManager: function( manager ) {
+				this.turnManager = manager;
+			},
+
 			setLastTargetPos: function(pos) {
 				//posの型が異なる場合は何もしない。
 				if(pos.x === undefined || pos.y === undefined) { return; }
@@ -109,7 +250,6 @@
 				isNextToLastPos = panel.isNextPanel( this.lastTargetPos );
 				if ( isSame && isNextToLastPos ) {
 					var dirs = this.getDirection( pos );
-					console.log( dirs );
 					this.drawArrow( this.lastTargetPos, dirs.lastPanel );
 					this.drawArrow( pos,                dirs.newPanel );
 					if( panel.setChained() ){
@@ -122,16 +262,18 @@
 			},
 
 			onTouchEnd: function(e) {
+				//ターン終了か、消えなかったときは、ターン中に戻る。
+				for ( var i = 0; i < this.drawnArrows.length; i++ ){
+					this.removeChild(this.drawnArrows[i]);
+				}
 				if ( this.chainedNum >= this.REMOVE_CHAINED_NUM ) {
 					for ( var i = 0; i < this.chainedPanels.length; i++ ){
 						this.chainedPanels[i].removePanel();
 					}
-					this.closeUp();
+					this.turnManager.endTurn();
 				}
-				for ( var i = 0; i < this.drawnArrows.length; i++ ){
-					this.removeChild(this.drawnArrows[i]);
-				}
-				this.clear();
+				this.turnManager.onCancelTouch();
+				//this.clear();
 			},
 
 			getTouchPosition: function( x, y ) {
@@ -143,7 +285,7 @@
 
 			//その位置が最後の位置に対してどの方角かを得る。
 			getDirection: function( pos ) {
-				var dir = new Direction();
+				var dir = DIRECTION;
 				if ( pos.x === this.lastTargetPos.x + 1 && pos.y === this.lastTargetPos.y     ) { return {lastPanel: dir.RIGHT,       newPanel: dir.LEFT }; }
 				if ( pos.x === this.lastTargetPos.x - 1 && pos.y === this.lastTargetPos.y     ) { return {lastPanel: dir.LEFT,        newPanel: dir.RIGHT }; }
 				if ( pos.x === this.lastTargetPos.x     && pos.y === this.lastTargetPos.y + 1 ) { return {lastPanel: dir.DOWN,        newPanel: dir.TOP }; }
@@ -177,23 +319,23 @@
 			//createPanel: function( panelType, pos ){
 			createPanel: function( panelType ){
 				switch( panelType ) {
-					case 1:
+					case PANEL_TYPE.COIN:
 						//return new Coin(PANEL_X_SIZE, PANEL_Y_SIZE, pos);
 						return new Coin( PANEL_X_SIZE, PANEL_Y_SIZE );
 						break;
-					case 2:
+					case PANEL_TYPE.SHIELD:
 						//return new Shield(PANEL_X_SIZE, PANEL_Y_SIZE, pos);
 						return new Shield( PANEL_X_SIZE, PANEL_Y_SIZE );
 						break;
-					case 3:
+					case PANEL_TYPE.SKELTON:
 						//return new Skelton(PANEL_X_SIZE, PANEL_Y_SIZE, pos);
 						return new Skelton( PANEL_X_SIZE, PANEL_Y_SIZE );
 						break;
-					case 4:
+					case PANEL_TYPE.SWORD:
 						//return new Sword(PANEL_X_SIZE, PANEL_Y_SIZE, pos);
 						return new Sword( PANEL_X_SIZE, PANEL_Y_SIZE );
 						break;
-					case 5:
+					case PANEL_TYPE.POTION:
 						//return new Sword(PANEL_X_SIZE, PANEL_Y_SIZE, pos);
 						return new Potion( PANEL_X_SIZE, PANEL_Y_SIZE );
 						break;
@@ -209,7 +351,6 @@
 					for (var y = 0; y < this.panelY; y++) {
 						var p = this.fields[x][y];
 						if ( p === null ) {
-							console.log( "hoge" );
 							this.fields[x].splice( y, 1 );
 							var type = parseInt( Math.random() * 5 ) + 1;
 							//p = this.createPanel( type, { x:x, y:0 } );
@@ -252,6 +393,19 @@
 						p.setPos( { x:x, y:y } );
 					}
 				}
+			},
+
+			getDamageFromEnemies: function() {
+				var damage = 0;
+				for (var x = 0; x < this.panelX; x++) {
+					for (var y = 0; y < this.panelY; y++) {
+						var p = this.fields[x][y];
+						if ( p.getPanelType() === PANEL_TYPE.SKELTON) {
+							damage += p.getDamage();
+						}
+					}
+				}
+				return damage;
 			}
 		});
 
@@ -320,7 +474,7 @@
 			initialize: function() {
 				Panel.apply( this, arguments );
 				this.image = game.assets[ 'img/panels3.png'];
-				this.panelType = 2;
+				this.panelType = PANEL_TYPE.COIN;
 				this.frame     = 1;
 			},
 		});
@@ -329,16 +483,20 @@
 			initialize: function() {
 				Panel.apply( this, arguments );
 				this.image = game.assets[ 'img/panels3.png'];
-				this.panelType = 3;
+				this.panelType = PANEL_TYPE.POTION;
 				this.frame     = 2;
+				this.restore   = 1;
 			},
+			getRestore: function() {
+				return this.restore();
+			}
 		});
 
 		var Shield    = Class.create( Panel, {
 			initialize: function() {
 				Panel.apply( this, arguments );
 				this.image = game.assets[ 'img/panels3.png'];
-				this.panelType = 4;
+				this.panelType = PANEL_TYPE.SHIELD;
 				this.frame  = 3;
 			},
 		});
@@ -347,16 +505,20 @@
 			initialize: function() {
 				Panel.apply( this, arguments );
 				this.image = game.assets[ 'img/panels3.png'];
-				this.panelType = 5;
+				this.panelType = PANEL_TYPE.SKELTON;
 				this.frame  = 4;
+				this.damage = 1;
 			},
+			getDamage: function() {
+				return this.damage;
+			}
 		});
 
 		var Sword    = Class.create( Panel, {
 			initialize: function() {
 				Panel.apply( this, arguments );
 				this.image = game.assets[ 'img/panels3.png'];
-				this.panelType = 6;
+				this.panelType = PANEL_TYPE.SWORD;
 				this.frame  = 5;
 			},
 		});
@@ -372,26 +534,15 @@
 			},
 		});
 
-		var Direction = Class.create({
-			initialize: function(){
-				this.RIGHT     = 0;
-				this.DOWN      = 1;
-				this.LEFT      = 2;
-				this.TOP       = 3;
-				this.RIGHTDOWN = 4;
-				this.LEFTDOWN  = 5;
-				this.LEFTTOP   = 6;
-				this.RIGHTTOP  = 7;
-			}
-		});
+
 
 
 		window.onload = function(){
 			//game.preload( 'img/skelton.png' );
 			game.onload = function() {
-				var gameScene  = new GameScene ( game, scoreScene );
-				var titleScene = new TitleScene( game, gameScene );
-				var scoreScene = new Scene();
+				//var gameScene  = new GameScene( game, gameOverScene );
+				var titleScene = new TitleScene( game );
+				//var gameOverScene = new GameOverScene( game, titleScene );
 				game.pushScene( titleScene );
 			}
 			game.start();
